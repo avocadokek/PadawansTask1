8a4ad88a658c02d442f124952e9c4683fec20764
avocadokek <47501889+avocadokek@users.noreply.github.com>
2019-02-16 11:32:17 +0200

    Add files via upload

--- Population.cs
+++ Population.cs
@@ -4,10 +4,31 @@ namespace PadawansTask1
 {
     public class Population
     {
+
+
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
+            int Years = 0;
+            double temp = Convert.ToDouble(initialPopulation);
+
+
+            while (currentPopulation > temp)
+            {
+                temp = temp * (1 + (percent / 100)) + Convert.ToDouble(visitors);
+                Years++;
+            }
+
+            return Years;
             throw new NotImplementedException();
         }
+
+        static public void Main()
+
+        {
+            int result = GetYears(1500, 5, 100, 55000);
+            Console.WriteLine($"Result is: {result} years");
+        }
     }
+
+
 }
